2e180b0fee8aac6adde7b735e89e4a9d348d297d
iampenoush <simonsoglo8@gmail.com>
2021-03-22 12:35:18 +0000

    J'ai ajouté un nouveau projet

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Course;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CourseController extends Controller
+{
+    public function index()
+    {
+        $courses = Course::with('user')
+        ->select('courses.*', DB::raw(
+            '(SELECT COUNT(DISTINCT(user_id))
+            FROM completionss
+            INNER JOIN episodes ON completionss.episode_id = episodes.id
+            WHERE episodes.course_id = courses.id
+            ) AS participants'
+        ))
+        ->withCount('episodes')->latest()->get();
+
+        return Inertia::render('Courses/Index', [
+            'courses' => $courses
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        $course = Course::where('id', $id)->with('episodes')->first();
+        $watched = auth()->user()->episodes;
+
+        return Inertia::render('Courses/show', [
+            'course' => $course,
+            'watched' => $watched
+        ]);
+    }
+
+    public function toggleProgress(Request $request)
+    {
+        $id = $request->input('episodeId');
+        $user = auth()->user();
+
+        $user->episodes()->toggle($id);
+
+        return $user->episodes;
+
+    }
+}
---
+++ resources/js/Pages/Courses/show.vue
@@ -0,0 +1,68 @@
+<template>
+    <app-layout>
+        <template #header>
+            {{course.title}}
+        </template>
+        <div class="py-4 mx-8">
+            <div class="text-2xl">{{ this.courseShow.episodes[this.currentkey].title }}</div>
+              <iframe class="w-full h-screen" :src="this.courseShow.episodes[this.currentkey].video_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="text-sm text-gray-500">{{ this.courseShow.episodes[this.currentkey].description }}</div>
+            <div class="py-6">
+                <progress-bar :watched-episodes ="watched" :episodes ="course.episodes"/>
+            </div>
+
+            <div class="mt-6">
+                <ul v-for="(episode, index) in this.courseShow.episodes" v-bind:key="episode.id">
+                    <li class="mt-3 flex justify-between items-center">
+                        <div>
+                            Episode n°{{ index + 1 }} - {{ episode.title }}
+                            <button class="text-gray-500 focus:text-indigo-500 focus:outline-none" 
+                            @click="switchEpisode(index)">Lire</button>
+                        </div>
+                        <progress-button :episode-id="episode.id" :watched-episodes="watched"/>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+
+import AppLayout from './../../Layouts/AppLayout';
+import ProgressButton from './ProgressButton'; 
+import ProgressBar from './ProgressBar'; 
+
+export default {
+    components: {
+        AppLayout,
+        ProgressButton,
+        ProgressBar
+    },
+
+    props: ['course', 'watched'],
+
+    data() {
+        return {
+            courseShow: this.course,
+            currentkey: 0
+        }
+    },
+
+    methods: {
+        switchEpisode(index){
+            this.currentkey = index;
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+        }
+    },
+
+    mounted() {
+        // console.log(this.courseList);
+    }
+    
+}
+</script>
